5392eb2edb8dbcdd5ab167d130b134427a697174
Augustin Riedinger <augustin.riedinger@gmail.com>
2023-09-14 11:30:37 +0200

    cache clear ok

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
-import express, { Express, Request, Response, NextFunction } from "express";
+import express, { Express, Request, Response } from "express";
+import cron from "node-cron";
 
 import { queryParamsCheck } from "./utils/middleware";
 import { fetchStormglassAPIWithCache, fetchFormattedStormglassAPI } from "./utils/api";
 import { generateCalendar } from "./utils/calendar";
 import { extractWindIntervals } from "./utils/weather";
+import { reset } from "./utils/cache";
 
 const PORT = process.env.PORT || 3000;
 
 const MIN_CONSECUTIVE_HOURS = 4; // Make it dynamic?
+
+cron.schedule("0 0 * * *", async () => {
+  console.info("Reseting cache files");
+  const deletedFiles = await reset();
+  if (deletedFiles.length) {
+    console.info(`${deletedFiles} deleted`);
+  }
+});
 
 const app: Express = express();
 
--- utils/cache.ts
+++ utils/cache.ts
@@ -1,6 +1,9 @@
 import { promises as fs } from "fs";
+import { getMSInterval } from "./calendar";
 
-const toCachePath = (filename: string): string => `./cache/${filename}.json`;
+const CACHE_PATH = "./cache";
+
+const toCachePath = (filename: string): string => `${CACHE_PATH}/${filename.replace(/.json$/i, "")}.json`;
 
 export const lastModifiedAt = async (filename: string): Promise<Date | null> => {
   try {
@@ -20,5 +23,17 @@ export const read = async (filename: string): Promise<Object> => {
   return JSON.parse((await fs.readFile(toCachePath(filename))).toString());
 };
 
-// TODO
-const reset = () => {};
+export const reset = async () => {
+  const files = await fs.readdir(CACHE_PATH);
+
+  const filesToDelete = await files.reduce(async (result, filename) => {
+    if (filename.endsWith(".json") && getMSInterval(await lastModifiedAt(filename)) > 1000 * 3600 * 24 * 7) {
+      return (await result).concat(filename);
+    }
+    return result;
+  }, Promise.resolve([]));
+
+  await Promise.all(filesToDelete.map(async (filename) => fs.unlink(toCachePath(filename))));
+
+  return filesToDelete;
+};
