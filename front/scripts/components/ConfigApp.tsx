faeb555669c217bb3c44b31437bb02e5a0c23323
Augustin Riedinger <augustin.riedinger@gmail.com>
2023-09-25 14:20:20 +0200

    almost done

--- front/scripts/components/ConfigApp.tsx
+++ front/scripts/components/ConfigApp.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Map from "./Map";
-import MultiRange from "./MultiRange/MultiRange";
+import Slider from "./Slider";
 
-const HOST_NAME = "https://wind-calendar.augustin-riedinger.fr";
+const HOST_NAME = window.location.origin;
 const GRUISSAN_COORDS = [43.09, 3.12];
 const DEFAULT_WIND_STRENGTHS = [15, 25, 50];
 
@@ -11,27 +11,68 @@ const App = () => {
   const [min, setMin] = useState(DEFAULT_WIND_STRENGTHS[0]);
   const [strong, setStrong] = useState(DEFAULT_WIND_STRENGTHS[1]);
   const [max, setMax] = useState(DEFAULT_WIND_STRENGTHS[2]);
+  const [isCopied, setIsCopied] = useState(false);
+  const setValue = [setMin, setStrong, setMax];
   const [lat, lng] = coords;
 
   const url = `${HOST_NAME}/cal?lat=${lat}&lng=${lng}&min=${min}&strong=${strong}&max=${max}`;
 
-  const onRangeChange = ({ min, max }) => {
-    setMin(min);
-    setMax(max);
+  const onRangeChange = (newValue) => {
+    newValue.forEach((v, i) => {
+      setValue[i](v);
+    });
   };
+
+  useEffect(() => {
+    setIsCopied(false);
+  }, [coords, min, strong, max]);
 
   return (
     <div>
       <p>
-        <strong>1. Place your spot on the map</strong>
+        <strong>1. Positionne l'icône sur ton spot</strong>
       </p>
       <div className="my-4">
         <Map coords={coords} setCoords={setCoords} initialZoom={13} />
       </div>
+      <p>
+        <strong>2. Ajuste la fenêtre de vent qui t'intéresse (en nœuds)</strong>
+      </p>
       <div className="my-5">
-        <MultiRange min={min} max={max} onChange={(e) => onRangeChange(e)} />
+        <Slider value={[min, strong, max]} min={0} max={75} onChange={onRangeChange} />
       </div>
-      <input className="form-control form-control-lg" value={url} readOnly></input>
+      <p>
+        <strong>3. Copie/colle le lien ci-dessous dans ton agenda</strong>
+      </p>
+      <div className="input-group">
+        <input
+          className="form-control form-control-lg"
+          id="output"
+          value={url}
+          readOnly
+          onClick={(event) => event.target.select()}
+        ></input>
+        <button
+          className="input-group-text"
+          title="Copier dans le presse-papier"
+          onClick={() => {
+            navigator.clipboard.writeText(document.getElementById("output").value);
+            setIsCopied(true);
+          }}
+        >
+          {isCopied ? (
+            <span>
+              <i className="bi-check"></i>
+              &nbsp;Copié
+            </span>
+          ) : (
+            <span>
+              <i className="bi-clipboard"></i>
+              &nbsp;Copier
+            </span>
+          )}
+        </button>
+      </div>
     </div>
   );
 };
---
+++ front/scripts/components/Slider.tsx
@@ -0,0 +1,57 @@
+import ReactSlider from "react-slider";
+
+import styled from "styled-components";
+
+const StyledSlider = styled(ReactSlider)`
+  width: 100%;
+  height: 20px;
+`;
+
+const StyledThumb = styled.div`
+  height: 30px;
+  line-height: 30px;
+  width: 30px;
+  text-align: center;
+  background-color: #000;
+  color: #fff;
+  border-radius: 50%;
+  cursor: grab;
+  top: -5px;
+  margin-bottom: 2rem;
+`;
+
+const Thumb = (props, state) => <StyledThumb {...props}>{state.valueNow}</StyledThumb>;
+
+const sliderLabel = (index) => {
+  if (index === 0) return "Trop faible";
+  if (index === 1) return "Vent modéré";
+  if (index === 2) return "Vent fort";
+  return "Trop fort";
+};
+
+const sliderBackground = (index) => {
+  if (index === 1) return "var(--bs-green)";
+  if (index === 2) return "var(--bs-orange)";
+  if (index === 3) return "rgba(255, 0, 0, 0.1)";
+  return "#ddd";
+};
+
+const StyledTrack = styled.div`
+  top: 0;
+  bottom: 0;
+  background: ${(props) => sliderBackground(props.index)};
+  border-radius: 999px;
+  text-align: center;
+  font-size: 0.75rem;
+  &:after {
+    content: "${(props) => sliderLabel(props.index)}";
+    position: relative;
+    bottom: -2rem;
+  }
+`;
+
+const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
+
+export default (props) => {
+  return <StyledSlider renderTrack={Track} renderThumb={Thumb} {...props} />;
+};
